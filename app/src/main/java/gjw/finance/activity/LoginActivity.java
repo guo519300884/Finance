d574df31941a38206d3031f86ac5eb8967dc354b
皇上 <519300884@qq.com>
2017-03-15 19:53:35 +0800

    12.实现注冊頁面实现注册功能

--- app/src/main/java/gjw/finance/activity/LoginActivity.java
+++ app/src/main/java/gjw/finance/activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package gjw.finance.activity;
 
+import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -8,6 +10,9 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -76,38 +81,53 @@ public class LoginActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.btn_login:
 
-                String phone = loginEtNumber.getText().toString().trim();
-                String pw = loginEtPwd.getText().toString().trim();
-                if (TextUtils.isEmpty(phone)) {
-                    Toast.makeText(this, "账号不能为空", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(pw)) {
-                    Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                Map<String, String> map = new HashMap<>();
-                map.put("phone", phone);
-                map.put("password", pw);
-                //去服务区登录
-                LoadNet.getDataPost(AppNetConfig.LOGIN, map, new LoadNetHttp() {
-
-                    @Override
-                    public void success(String context) {
-                        Toast.makeText(LoginActivity.this, "success" + context, Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void failure(String error) {
-                        Toast.makeText(LoginActivity.this, "error" + error, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
+                login();
                 break;
             case R.id.login_regitster_tv:
-
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
                 break;
         }
+    }
+
+    private void login() {
+        String phone = loginEtNumber.getText().toString().trim();
+        String pw = loginEtPwd.getText().toString().trim();
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "账号不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pw)) {
+            Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Map<String, String> map = new HashMap<>();
+        map.put("phone", phone);
+        map.put("password", pw);
+        //去服务区登录
+        LoadNet.getDataPost(AppNetConfig.LOGIN, map, new LoadNetHttp() {
+
+            @Override
+            public void success(String context) {
+                Log.e("TAG", "LoginActivity success()" + context);
+                Toast.makeText(LoginActivity.this, "success" + context, Toast.LENGTH_SHORT).show();
+                JSONObject jsonObject = JSON.parseObject(context);
+                Log.e("TAG", "LoginActivity success()" + jsonObject);
+                Boolean success = jsonObject.getBoolean("success");
+                if (success) {
+                    //成功就解析数据
+//                            JSON.parseObject(context,);
+
+                }
+
+
+            }
+
+            @Override
+            public void failure(String error) {
+                Toast.makeText(LoginActivity.this, "error" + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/gjw/finance/activity/RegisterActivity.java
@@ -0,0 +1,121 @@
+package gjw.finance.activity;
+
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.InjectView;
+import butterknife.OnClick;
+import gjw.finance.R;
+import gjw.finance.utils.AppNetConfig;
+import gjw.finance.utils.LoadNet;
+import gjw.finance.utils.LoadNetHttp;
+
+public class RegisterActivity extends BaseActivity {
+
+    @InjectView(R.id.base_title)
+    TextView baseTitle;
+    @InjectView(R.id.base_back)
+    ImageView baseBack;
+    @InjectView(R.id.base_setting)
+    ImageView baseSetting;
+    @InjectView(R.id.et_register_number)
+    EditText etRegisterNumber;
+    @InjectView(R.id.et_register_name)
+    EditText etRegisterName;
+    @InjectView(R.id.et_register_pwd)
+    EditText etRegisterPwd;
+    @InjectView(R.id.et_register_pwdagain)
+    EditText etRegisterPwdagain;
+    @InjectView(R.id.btn_register)
+    Button btnRegister;
+
+    @Override
+    public void initTitle() {
+        baseTitle.setText("注册");
+        baseSetting.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_register;
+    }
+
+    @OnClick({R.id.base_back, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.base_back:
+                finish();
+                break;
+            case R.id.btn_register:
+                //校验
+                String number = etRegisterNumber.getText().toString().trim();
+                String name = etRegisterName.getText().toString().trim();
+                String pwd = etRegisterPwd.getText().toString().trim();
+                String pwda = etRegisterPwdagain.getText().toString().trim();
+
+                //判空
+                if (TextUtils.isEmpty(name) || TextUtils.isEmpty(number)
+                        || TextUtils.isEmpty(pwd) || TextUtils.isEmpty(pwda)) {
+
+                    Toast.makeText(this, "不能有空的呀", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                Map<String, String> map = new HashMap<>();
+                map.put("number", number);
+                map.put("name", name);
+                map.put("pwd", pwd);
+                LoadNet.getDataPost(AppNetConfig.REGISTER, map, new LoadNetHttp() {
+                    @Override
+                    public void success(String context) {
+                        JSONObject jsonObject = JSON.parseObject(context);
+                        Boolean isExist = jsonObject.getBoolean("isExist");
+
+                        if (isExist) {
+                            Toast.makeText(RegisterActivity.this, "此账号已存在", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void failure(String error) {
+
+                    }
+                });
+
+                break;
+        }
+    }
+
+    public void isEmpty(String name, String alert) {
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(this, alert, Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+}
