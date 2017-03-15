a618f03a9a606fd723bc9641bba5086d9485c7dc
皇上 <519300884@qq.com>
2017-03-15 21:18:09 +0800

    13.显示个人中心页面 & 设置用户名 & 设置头像

--- app/src/main/java/gjw/finance/fragment/PropertFragment.java
+++ app/src/main/java/gjw/finance/fragment/PropertFragment.java
@@ -1,26 +1,48 @@
 package gjw.finance.fragment;
 
-import android.view.View;
+import android.graphics.Bitmap;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import gjw.finance.R;
+import gjw.finance.activity.MainActivity;
+import gjw.finance.bean.UserInfo;
 import gjw.finance.utils.AppNetConfig;
+import gjw.finance.utils.BitmapUtils;
 
 /**
  * Created by 皇 上 on 2017/3/10.
  */
 
 public class PropertFragment extends BaseFragment {
-    @InjectView(R.id.base_title)
-    TextView baseTitle;
-    @InjectView(R.id.base_back)
-    ImageView baseBack;
-    @InjectView(R.id.base_setting)
-    ImageView baseSetting;
-    private View view;
+
+
+    @InjectView(R.id.tv_settings)
+    TextView tvSettings;
+    @InjectView(R.id.iv_me_icon)
+    ImageView ivMeIcon;
+    @InjectView(R.id.rl_me_icon)
+    RelativeLayout rlMeIcon;
+    @InjectView(R.id.tv_me_name)
+    TextView tvMeName;
+    @InjectView(R.id.rl_me)
+    RelativeLayout rlMe;
+    @InjectView(R.id.recharge)
+    ImageView recharge;
+    @InjectView(R.id.withdraw)
+    ImageView withdraw;
+    @InjectView(R.id.ll_touzi)
+    TextView llTouzi;
+    @InjectView(R.id.ll_touzi_zhiguan)
+    TextView llTouziZhiguan;
+    @InjectView(R.id.ll_zichan)
+    TextView llZichan;
 
     @Override
     protected int getLayoutId() {
@@ -30,16 +52,34 @@ public class PropertFragment extends BaseFragment {
     @Override
     protected void initData(String json) {
 
-        baseTitle.setText("国库");
-        baseBack.setVisibility(View.INVISIBLE);
-        baseSetting.setVisibility(View.INVISIBLE);
+        MainActivity mainActivity = (MainActivity) getActivity();
+        UserInfo user = mainActivity.getUser();
+        //设置用户名
+        tvMeName.setText(user.getData().getName());
+        //设置头像
+        Picasso.with(getContext()).load(AppNetConfig.BASE_URL + "/images/tx.png")
+                .transform(new Transformation() {
+                    @Override
+                    public Bitmap transform(Bitmap source) {
 
+                        Bitmap circleBitmap = BitmapUtils.circleBitmap(source);
+
+                        source.recycle();
+
+                        return circleBitmap;
+                    }
+
+                    @Override
+                    public String key() {
+                        return "";
+                    }
+                }).into(ivMeIcon);
 
     }
 
     @Override
     protected String getChildUrl() {
-        return AppNetConfig.INDEX;
+        return null;
     }
 
     @Override
@@ -52,4 +92,5 @@ public class PropertFragment extends BaseFragment {
         super.onDestroyView();
         ButterKnife.reset(this);
     }
+
 }
